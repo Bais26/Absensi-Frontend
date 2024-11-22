dc6d745832a06aa457f3719a7170cbb10df2dae5
Bais26 <baisyufan2004@gmail.com>
2024-11-22 21:27:45 +0700

    setup config text colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary_bg: '#181818',
+        second_bg: '#232427',
+        aksen_bg: '#DA0138',
+        primary_text: '#C8C8C9',
+        second_text: '#232427',
+        aksen_text: '#DA0138',
+        primary_card: '#47484D',
+        second_card: '#47484D',
+      },
+    },
   },
   plugins: [],
 }
